b0c7bc491732e3281972172e6cde034388dae4a9
Vinicius Abade <vinicius@kenzie.com.br>
2021-01-19 00:34:31 -0300

    feat: change project setup

---
+++ backend/tests/test_initial_setup.py
@@ -0,0 +1,16 @@
+from pytest import fail
+
+
+class TestBaseApplication:
+    def test_factory_function_exists(self):
+        try:
+            __import__('app').create_app
+        except AttributeError:
+            fail('Make sure create_app function is defined')
+
+
+    def test_factory_function_return_flask_instance(self):
+        given = __import__('app').create_app()
+        expected = __import__('flask').Flask
+
+        assert isinstance(given, expected)
